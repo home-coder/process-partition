79aff3ffa6ae8e76ce6a601f9afa8b809f55be3a
jiangxiujie <jiangxiujie@cantv.cn>
2017-07-14 12:48:07 +0800

    完成对字段root为例子的解析过程,目前将 数据存储在一个字符串中

---
+++ Android.mk
@@ -0,0 +1,17 @@
+SUBDIR_MAKEFILES := $(call all-named-subdir-makefiles,modules tests)
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SHARED_LIBRARIES := libcutils liblog
+
+LOCAL_INCLUDES += $(LOCAL_PATH)
+
+LOCAL_CFLAGS  += -DQEMU_HARDWARE
+QEMU_HARDWARE := true
+
+LOCAL_SRC_FILES += yang.c
+
+LOCAL_MODULE:= yang
+include $(BUILD_EXECUTABLE)
+
--- yang.c
+++ yang.c
@@ -4,19 +4,81 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-#define PARTTIONLINE = "/proc/partitions"
+#ifdef ANDROID
+#include <android/log.h>
+#endif
+
+#include <string.h>
+#include <errno.h>
+
+#ifdef LOG_TAG
+#undef LOG_TAG
+#define LOG_TAG "Parttion Test"
+#endif
+
+#ifdef ANDROID
+#define  LOGI(...) __android_log_print (ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define  LOGE(...) __android_log_print (ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#endif
+
+#define PARTTIONLINE  "/proc/partitions"
+#define COMDLINE	  "/proc/cmdline"
+#define BUFSIZE	      256
+const char* time = __DATE__" " __TIME__;
+const char* dbinfo = "__LINE__,__func__";
 
 int main()
 {
+#ifdef PARTTION
 	int fd;
-
 	fd = open(PARTTIONLINE, O_RDONLY);
 	if (fd < 0) {
 		perror("open failed!\n");
 		exit(-1);
 	}
+#endif
 
+#ifdef COMDLINE
+	char *rootkey = "root";
+	char buf[BUFSIZE];
+	char *rootloc = NULL;
+	char *valueloc = NULL;
+	char rootvalue[BUFSIZE];
+	int ret = 0;
+
+	int fd;
+	fd = open(COMDLINE, O_RDONLY);
+	if (fd < 0) {
+		perror("open failed!\n");
+		exit(-1);
+	}
 	
+	if ((ret = read(fd, buf, BUFSIZE)) < 0) {
+		fprintf(stderr, "read err");
+		exit(-1);
+	}
+	buf[ret] = '\0';
+	printf("--buf:%s\n", buf);
+	rootloc = strstr(buf, rootkey);
+	if (rootloc) {
+		printf("--rootloc:%s\n", rootloc);
+	} else {
+		perror("--rootloc is null\n");
+		exit(-1);
+	}
+
+	valueloc = rootloc + strlen(rootkey) - 1;
+	if (!valueloc) {
+		perror("--valueloc is not exsit\n");
+		return -1;
+	}
+	memset(rootvalue, 0, BUFSIZE);
+	memccpy(rootvalue, valueloc, ' ', BUFSIZE);
+	ret = strlen(rootvalue);
+	printf("--ret:%d\n", ret);
+	rootvalue[ret] = '\0';
+	printf("rootvalue:%s\n", rootvalue);
+#endif
 
 	return 0;
 }
